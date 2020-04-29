1f9056abdc57163798244d583a4947c48ce5c3ef
Daniil Kiyanov <therandom@ya.ru>
2020-04-29 19:46:14 +0300

    подправил валидацию

--- scripts/components/TradeWidget/TradeWidget.js
+++ scripts/components/TradeWidget/TradeWidget.js
@@ -14,8 +14,17 @@ export class TradeWidget {
         this._balance = balance;
         this._currentItem = item;
         this._total = item.price_usd * 0;
+
         this._modal.open();
+
+        this._wasFormTouched = false;
         this._updateDisplay();
+    }
+
+    _updateDisplay() {
+        this._updateAmount();
+        this._innerEls.title.textContent = this._currentItem.name;
+        this._innerEls.currentPrice.textContent = this._currentItem.price_usd;
     }
 
     _updateAmount(value = 0) {
@@ -23,23 +32,31 @@ export class TradeWidget {
         this._total = this._currentItem.price_usd * this._amount;
         this._innerEls.total.textContent = this._total;
 
-        this._reportAmountError();
-    }
-
-    _updateDisplay() {
-        this._updateAmount();
-        this._innerEls.title.textContent = this._currentItem.name;
-        this._innerEls.currentPrice.textContent = this._currentItem.price_usd;
-        this._innerEls.amountInp.value = "";
+        if (this._wasFormTouched) {
+            this._reportAmountError();
+        }
     }
 
     _reportAmountError() {
-        this._isAmountError = this._total > this._balance || this._amount === 0;
+        console.log(this._amount)
+        const amountOverflow =  this._total > this._balance;
+        const amountZero =      this._amount === 0;
+        this._isAmountError = amountOverflow || amountZero;
+
+        let errorMsg = "";
+        if (this._isAmountError) {
+            if (amountZero) {
+                errorMsg = "Please enter a value you want to buy.";
+            }
+            else {
+                errorMsg = "Total sum is more than yor balance.";
+            }
+        }
 
         this._innerEls.amountInp.classList.toggle("invalid", this._isAmountError);
 
         this._innerEls.formFeedback.classList.toggle("error", this._isAmountError);
-        this._innerEls.formFeedback.textContent = this._isAmountError ? "Total sum is more than yor balance.": "";
+        this._innerEls.formFeedback.textContent = errorMsg;
 
         this._innerEls.buyBtn.disabled = this._isAmountError;
 
@@ -47,7 +64,17 @@ export class TradeWidget {
     }
 
     _reportValidity() {
-        return this._innerEls.form.reportValidity() && this._reportAmountError();;
+        return this._reportAmountError() && this._innerEls.form.checkValidity();
+    }
+
+    _clearFormReports() {
+        this._isAmountError = false;
+
+        this._innerEls.amountInp.value = "";
+        this._innerEls.amountInp.classList.remove("invalid");
+        this._innerEls.amountLabel.classList.remove("active");
+        this._innerEls.formFeedback.textContent = "";
+        this._innerEls.formFeedback.classList.remove("error");
     }
 
     _render() {
@@ -63,7 +90,7 @@ export class TradeWidget {
                         <form class="buy-form col s12" id="buy-form" action="/" method="post">
                             <div class="input-field col s4 buy-form__inner">
                                 <input class="widget__amount-inp" id="amount" type="number" min="0.000000000001" step="0.000000000001" required>
-                                <label for="amount">Amount</label>
+                                <label class="amount-label" for="amount">Amount</label>
                             </div>
                             <div class="widget__form-feedback"></div>
                         </form>
@@ -85,41 +112,40 @@ export class TradeWidget {
             currentPrice:   this._el.querySelector(".widget__current-price"),
             total:          this._el.querySelector(".widget__total"),
             amountInp:      this._el.querySelector(".widget__amount-inp"),
+            amountLabel:    this._el.querySelector(".amount-label"),
             formFeedback:   this._el.querySelector(".widget__form-feedback"),
         };
 
-        this._modal = M.Modal.init(this._innerEls.modal);
+        this._modal = M.Modal.init(this._innerEls.modal, {
+            onCloseStart: () => this._clearFormReports(),
+        });
 
         this._el.addEventListener("input", event => {
+            this._wasFormTouched = true;
+
             const value = +event.target.value;
             this._updateAmount(value);
         });
 
         this._el.addEventListener("click", event => {
-            const isBuyBtn =    event.target === this._innerEls.buyBtn;
-            const isCloseBtn =  event.target === this._innerEls.closeBtn;
-
-            if (isBuyBtn && this._reportValidity()) {
-                this._onBuyCallback(this._currentItem, this._amount);
-                this._modal.close();
-            }
-
-            if (isCloseBtn) {
+            if (event.target === this._innerEls.closeBtn) {
                 this._modal.close();
             }
         });
 
-        this._el.addEventListener("submit", event => {
-            event.preventDefault();
-
-            const form = event.target;
-
-            if (form.checkValidity()) {
-                ;
-            }
-            else {
-                this._reportAmountError();
-            }
+        this._innerEls.form.addEventListener("submit", event => {
+            this._onSubmitHandler(event);
         });
+    }
+
+    _onSubmitHandler(event) {
+        event.preventDefault();
+
+        this._wasFormTouched = true;
+
+        if (this._reportValidity()) {
+            this._onBuyCallback(this._currentItem, this._amount);
+            this._modal.close();
+        }
     }
 }
